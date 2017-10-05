96bb2d6c51a66a7213a9c2c0269f2f9a21a571f0
Kevin Mui <kevinmui2020@u.northwestern.edu>
2017-10-05 02:23:06 -0500

    Update Scanner.js

--- app/components/Scanner.js
+++ app/components/Scanner.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 
 import {
-  Text,
-  View,
-  Linking,
-  Vibration,
-  Dimensions,
-  AlertIOS
+    Text,
+    View,
+    Linking,
+    Vibration,
+    Dimensions,
+    AlertIOS
 } from 'react-native';
 
 import Camera from 'react-native-camera';
@@ -15,39 +15,39 @@ import styles from '../styles/scannerstyles';
 
 export default class Scanner extends Component {
 
-  constructor(props) {
-      super(props);
-      this.state = {
-          scanning: true
-      };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            scanning: true
+        };
+    }
 
-  render() {
-    if (this.state.scanning) {
-      return (
-        <View style={styles.container}>
-          <Camera
-            ref={(cam) => {
-              this.camera = cam;
-            }}
-            style={styles.preview}
-            aspect={Camera.constants.Aspect.fill}
-            onBarCodeRead={this._onBarCodeRead.bind(this)}
-            >
-          </Camera>
-        </View>
-      );
+    render() {
+      if (this.state.scanning) {
+        return (
+          <View style={styles.container}>
+            <Camera
+              ref={(cam) => {
+                this.camera = cam;
+              }}
+              style={styles.preview}
+              aspect={Camera.constants.Aspect.fill}
+              onBarCodeRead={this._onBarCodeRead.bind(this)}
+              >
+            </Camera>
+          </View>
+        );
+      }
+      else {
+        return (
+          <View style={styles.scan_container}>
+            <Text style={styles.item}>
+              Go Back!
+            </Text>
+          </View>
+        );
+      }
     }
-    else {
-      return (
-        <View style={styles.scan_container}>
-          <Text style={styles.item}>
-            Go Back!
-          </Text>
-        </View>
-      );
-    }
-  }
 
     _onBarCodeRead(e) {
         Vibration.vibrate();
@@ -56,5 +56,4 @@ export default class Scanner extends Component {
             "ID: " + e.data
         );
     }
-
 }
