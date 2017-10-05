b9700d489e2b2beb485eb8e18564ffb1399a3daa
Kevin Mui <kevinmui2020@u.northwestern.edu>
2017-10-05 02:21:00 -0500

    Update scannerstyles.js

--- app/styles/scannerstyles.js
+++ app/styles/scannerstyles.js
@@ -1,4 +1,4 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions, } from 'react-native';
 
 export default StyleSheet.create({
     container: {
@@ -6,5 +6,37 @@ export default StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         backgroundColor: "transparent",
-    }
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        color: '#000',
+        padding: 10,
+        margin: 40
+    },
+    scan_container: {
+        flex: 1,
+        padding: 8,
+        flexDirection: 'column', // main axis
+        justifyContent: 'center', // main axis
+        alignItems: 'center', // cross axis
+        backgroundColor: '#ED8580',
+    },
+    item: {
+        color: '#FFF',
+        includeFontPadding: false,
+        fontSize: 30,
+        padding: 10,
+        height: 44,
+        marginBottom: 10,
+        fontFamily: 'Menlo',
+    },
 });
