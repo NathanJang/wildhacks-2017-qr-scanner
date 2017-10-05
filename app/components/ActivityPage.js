ff5310dd3f0cc06a3b8248bf3e2d159007747cab
Kevin Mui <kevinmui2020@u.northwestern.edu>
2017-10-05 02:16:31 -0500

    Update ActivityPage.js

--- app/components/ActivityPage.js
+++ app/components/ActivityPage.js
@@ -11,42 +11,84 @@ import {
 
 import styles from '../styles/activityView';
 
-export default class ActivityView extends Component {
+import Scanner from './Scanner';
 
+export default class ActivityView extends Component {
+    
     constructor(props) {
         super(props);
         this.state = {
             data: [
                 {key: 'Lightning Talks'},
-                {key: 'Karaoke'},
-                {key: 'Swing Dancing'},
-                {key: 'Beat Boxing'},
-                {key: 'Dodgeball'},
+                {key: 'Super Smash Tournament'},
+                {key: 'Cup Stacking'}
             ]
         };
+        this.handleScan1 = this.handleScan1.bind(this);
+        this.handleScan2 = this.handleScan2.bind(this);
+        this.handleScan3 = this.handleScan3.bind(this);
     }
 
-    renderItem = ({item}) => (
-        <TouchableHighlight
-          onPress={this.handleMeals}
-        >
-          <View style={styles.activity_rows}>
-            <Text style={styles.item}>
-              {item.key}
-            </Text>
-          </View>
+    handleScan1() {
+        this.props.navigator.push({
+            title: 'Lighning Talks',
+            component: Scanner,
+            passProps: {myElement: 'Lightning'}
+        });
+    }
 
-        </TouchableHighlight>
-    )
+    handleScan2() {
+        this.props.navigator.push({
+            title: 'Super Smash Tournament',
+            component: Scanner,
+            passProps: {myElement: 'Smash'}
+        });
+    }
 
+    handleScan3() {
+        this.props.navigator.push({
+            title: 'Cup Stacking',
+            component: Scanner,
+            passProps: {myElement: 'Stacking'}
+        });
+    }
 
     render() {
-        return (<ScrollView contentContainerStyle={styles.container}>
-          <FlatList
-              data={this.state.data}
-              renderItem={this.renderItem}
-          />
+        return (<View style={styles.container}>
+          <TouchableHighlight
+              onPress={this.handleScan1}
+          >
+              <View style={styles.row}>
+                  <Text style={styles.item}>
+                      Lightning
+                  </Text>
+              </View>
+          </TouchableHighlight>
 
-        </ScrollView>);
+          <Text style={styles.space}></Text>
+
+          <TouchableHighlight
+            onPress={this.handleScan2}
+          >
+              <View style={styles.row}>
+                  <Text style={styles.item}>
+                      Super Smash
+                  </Text>
+              </View>
+          </TouchableHighlight>
+
+          <Text style={styles.space}></Text>
+
+          <TouchableHighlight
+            onPress={this.handleScan3}
+          >
+              <View style={styles.row}>
+                  <Text style={styles.item}>
+                      Cup Stacking
+                  </Text>
+              </View>
+          </TouchableHighlight>
+        </View>);
     }
 }
+
